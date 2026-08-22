8a6828d036232adf1b757b9428c9451bbc67f779
Mauro Ponce <mgp1989@gmail.com>
2026-08-22 16:43:51 -0300

    Add tenant-scoped queries for Api::V1::Appointments

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,31 +1,32 @@
 class Api::V1::AppointmentsController < Api::V1::BaseController
 
   def index
-    appointments = Appointment.order(:starts_at)
+    appointments = current_account.appointments.order(:starts_at)
     render json: appointments
   end
 
   def show
-    appointment = Appointment.find(params[:id])
+    appointment = current_account.appointments.find(params[:id])
     render json: appointment
   end
 
   def create
-    appointment = Appointment.create!(appointment_params)
+    appointment = current_account.appointments.create!(scoped_appointment_params)
     
     render json: appointment, status: :created
   end
 
   def update
-    appointment = Appointment.find(params[:id])
-    appointment.update!(appointment_params)
+    appointment = current_account.appointments.find(params[:id])
+    appointment.update!(scoped_appointment_params)
+  
     render json: appointment
   end
 
   private
+
   def appointment_params
     params.require(:appointment).permit(
-      :account_id,
       :customer_id,
       :professional_id,
       :service_id,
@@ -33,5 +34,14 @@ class Api::V1::AppointmentsController < Api::V1::BaseController
       :status,
       :starts_at
     )
+  end
+
+  def scoped_appointment_params
+    scoped_params = appointment_params
+    scoped_params[:customer_id] = current_account.customers.find(scoped_params[:customer_id]).id
+    scoped_params[:professional_id] = current_account.professionals.find(scoped_params[:professional_id]).id
+    scoped_params[:service_id] = current_account.services.find(scoped_params[:service_id]).id
+
+    scoped_params
   end
 end
--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token
-  # Common functionality for API controllers can be added here
+  before_action :set_current_account
+
+  attr_reader :current_account
+
+  def set_current_account
+    @current_account = Account.find(request.headers['X-Account-Id'])
+  end
 end
