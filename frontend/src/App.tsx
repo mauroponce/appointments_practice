f192799faa01733525f668f0459530e36b8293a8
Mauro Ponce <mgp1989@gmail.com>
2026-08-24 18:42:34 -0300

    Type Appointment state with TypeScript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 import './App.css'
+import type { Appointment } from './types/appointment'
 
 function App() {
-  const [appointments, setAppointments] = useState([])
+  const [appointments, setAppointments] = useState<Appointment[]>([])
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     console.log("Fetching appointments...")
@@ -14,12 +16,17 @@ function App() {
     .then(response => response.json())
     .then(data => {
       setAppointments(data)
+      setLoading(false)
       console.log(data)
     })
     .catch(error => {
       console.error("FETCH FAILED:", error)
     })
   }, []) // With [], useEffect runs only once when the component mounts
+
+  if(loading) {
+    return <p>Loading appointments...</p>
+  }
 
   return (
     <>
@@ -31,7 +38,8 @@ function App() {
           <ul>
             {appointments.map(appointment => (
               <li key={appointment.id}>
-                <span>${appointment.price_cents}</span> - 
+                <span>${appointment.price_cents}</span>
+                {" - "}
                 <span>{appointment.status}</span>
               </li>
             ))}
---
+++ frontend/src/types/appointment.ts
@@ -0,0 +1,12 @@
+export interface Appointment {
+  account_id: number
+  created_at: string
+  customer_id: number
+  id: number
+  price_cents: number
+  professional_id: number
+  service_id: number
+  starts_at: string
+  status: string
+  updated_at: string
+}
